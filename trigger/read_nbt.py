1fcbe0e507290095f3736abc99195768c11de4c4
Bob <Megabobster@gmail.com>
2017-03-13 22:06:19 -0700

    Holy crap, NBT/JSON parsing!

--- trigger/read_nbt.py
+++ trigger/read_nbt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import sys
-nbtdata = sys.argv[1]
+import sys, json
+nbtdata = json.loads(sys.argv[1])
 
 # triggers when entitydata <entity> {} is run, which spits out the entity's full NBT data
 # variables are as follows
-# The data tag did not change: "$nbtdata"
+# The data tag did not change: "nbtdata"
 
-print "say " + str(nbtdata)
+if nbtdata["CustomName"] == "xyz":
+	print "say " + str(nbtdata["Pos"])
