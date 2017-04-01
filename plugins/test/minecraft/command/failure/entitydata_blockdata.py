8d979a6308afc2e443a016ab29fba94bb67a5938
Bob <Megabobster@gmail.com>
2017-04-01 15:50:09 -0700

    Fixed crazy hacked-together python scripts. Added exit code stuff to all remaining plugin scripts.

--- plugins/test/minecraft/command/failure/entitydata_blockdata.py
+++ plugins/test/minecraft/command/failure/entitydata_blockdata.py
@@ -9,19 +9,20 @@ def wrapper(command):
 	print "wrapper", command
 
 nbt_data = json.loads(sys.argv[1])
+plugin_exit_code = 0
 
 # triggers when entitydata <entity> {} is run, which spits out the entity's full NBT data
 # variables are as follows
 # The data tag did not change: "nbt_data"
 
 # Block data
-try:
-	if nbt_data["id"]:
-		mc("say " + str(nbt_data["x"]) + " " + str(nbt_data["y"]) + " " + str(nbt_data["z"]))
+if nbt_data.get("id"): # I think only blocks have the "id" tag, and all blocks should have it
+	mc("say " + str(nbt_data["x"]) + " " + str(nbt_data["y"]) + " " + str(nbt_data["z"]))
 # Entity data
-except KeyError:
-	try:
-		if nbt_data["CustomName"] == "xyz":
-			mc("say " + str(nbt_data["Pos"][0]) + " " + str(nbt_data["Pos"][1]) + " " + str(nbt_data["Pos"][2]))
-	except KeyError:
-		pass
+elif nbt_data.get("Pos"): # I think all entities have the "Pos" tag
+	if nbt_data.get("CustomName") == "xyz":
+		mc("say " + str(nbt_data["Pos"][0]) + " " + str(nbt_data["Pos"][1]) + " " + str(nbt_data["Pos"][2]))
+else:
+	plugin_exit_code = 1
+
+exit(plugin_exit_code)
--- plugins/test/minecraft/command/success/entitydata.py
+++ plugins/test/minecraft/command/success/entitydata.py
@@ -10,17 +10,18 @@ import sys, json
 
 executer = sys.argv[1]
 nbt_data = json.loads(sys.argv[2])
+plugin_exit_code=0
 
 # triggers when an entity's entitydata is successfully updated via the entitydata command
 # variables are as follows
 # [executer: Entity data updated to: nbt_data]
 
-try:
-	if nbt_data["CustomName"] == "xyz":
-		mc("say " + " " str(nbt_data["Pos"][0]) + " " + str(nbt_data["Pos"][1]) + " " + str(nbt_data["Pos"][2]))
-except KeyError:
-	try:
-		if "blend" in nbt_data["Tags"]:
-			mc("say test")
-	except KeyError:
-		pass
+if nbt_data.get("CustomName") == "xyz":
+	mc("say " + str(nbt_data["Pos"][0]) + " " + str(nbt_data["Pos"][1]) + " " + str(nbt_data["Pos"][2]))
+elif nbt_data.get("Tags"):
+	if "blend" in nbt_data.get("Tags"):
+		mc("say test")
+else:
+	plugin_exit_code = 1
+
+exit(plugin_exit_code)
